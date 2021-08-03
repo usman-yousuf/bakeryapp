3632859c998a893b52d1f234cccaef13ed92edf3
usman yousaf <uyousaf709@gmail.com>
2021-08-03 14:25:01 +0500

    fixes

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,7 +1,7 @@
 <html lang="en">
 
 <head>
-    
+
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -15,7 +15,7 @@
 
 
     <!-- custom stylesheet -->
-    <link rel="stylesheet" href="{{asset('assets/css/stylesheet.css')}}">
+    <link rel="stylesheet" href="{{asset('assets\css\stylesheet.css')}}">
 
 </head>
 
