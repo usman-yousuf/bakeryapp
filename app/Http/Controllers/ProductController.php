475c32e66e0edaf1ed7d187d5512dd76fe0d6561
Aleem71 <aleem@kodextech.com>
2021-08-13 13:06:53 +0500

    chnages

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -80,7 +80,6 @@ class ProductController extends Controller
                     return sendError('Admin Ingredient type does not exist or dont belongs to the same Admin Ingredient',[]);
 
                 $purchase_list = Purchaselist::where('user_id',$product->user_id)->where('admin_ingredient_id',$admin_ingredient->id)->where('admin_ingredient_type_id',$admin_ingredient_type->id)->first();
-                dd($admin_ingredient->id,$admin_ingredient_type->id);
                 $product_ingredient = new ProductIngredient;
                 $product_ingredient->product_id = $product->id;
                 $product_ingredient->purchase_list_id = $purchase_list->id ?? NULL;
