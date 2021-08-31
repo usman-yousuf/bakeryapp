a56556479f23568e24758f8b7e64ae8a20d2e98d
Aleem71 <aleem@kodextech.com>
2021-08-31 11:33:06 +0500

    cahnges

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -21,21 +21,21 @@ class OrderController extends Controller
 
         $validator = Validator::make($request->all(),[
 
-            'client_name' => 'required_without:order_id|string',
-            'phone_number' => 'required_without:order_id|numeric',
-            'delivery_address' => 'required_without:order_id|string',
-            'user_id' => 'required_without:order_id|exists:users,id',
-            'product_id' => 'required_without:order_id|exists:products,id',
+            'client_name'           => 'required_without:order_id|string',
+            'phone_number'          => 'required_without:order_id|numeric',
+            'delivery_address'      => 'required_without:order_id|string',
+            'user_id'               => 'required_without:order_id|exists:users,id',
+            'product_id'            => 'required_without:order_id|exists:products,id',
             'admin_product_type_id' => 'required_without:order_id|exists:admin_product_types,id',
-            'order_status' => 'required_with:order_id,product_id|in:new-order,in-baking,ready-to-deliver,sold,other',
-            'advance_payment' => 'required_without:order_id|numeric',
-            'order_id' => 'numeric|exists:orders,id',
-            'quantity' => 'required_without:order_id|numeric',
-            'phone_code' => 'string',
-            'raw_material' => 'string',
-            'tax' => 'required_with:raw_material|numeric',
-            'packing' => 'required_with:raw_material|numeric',
-            'profit' => 'required_with:raw_material|numeric',
+            'order_status'          => 'required_with:order_id,product_id|in:new-order,in-baking,ready-to-deliver,sold,other',
+            'advance_payment'       => 'required_without:order_id|numeric',
+            'order_id'              => 'numeric|exists:orders,id',
+            'quantity'              => 'required_without:order_id|numeric',
+            'phone_code'            => 'string',
+            'raw_material'          => 'string',
+            'tax'                   => 'required_with:raw_material|numeric',
+            'packing'               => 'required_with:raw_material|numeric',
+            'profit'                => 'required_with:raw_material|numeric',
 
         ]);
 
@@ -49,65 +49,67 @@ class OrderController extends Controller
         if(NULL == $order)
             $order = new Order;
 
-        if(!isset($request->order_id)){
-
-
         $admin_product_type = AdminProductType::where('id',$request->admin_product_type_id)->first();
+
         if(NULL == $admin_product_type)
             return sendError('Admin Product Type Does Not Exist',[]);
+
         $product = 0;
+
         $product = Product::where('id',$request->product_id)->where('admin_product_type_id',$admin_product_type->id)->first();
         if(NULL == $product)
             return sendError('Product Does Not Exist or Type Dont belongs to the product',[]);
-        }
+        
 
         if(isset($request->raw_material) || isset($request->order_id)){
             
-            $order->product_id = $product->id ?? $order->product_id;
-            $order->user_id = $request->user_id ?? $order->user_id;
+            $order->product_id            = $product->id ?? $order->product_id;
+            $order->user_id               = $request->user_id ?? $order->user_id;
             $order->admin_product_type_id = $admin_product_type->id ?? $order->admin_product_type_id;
-            $order->client_name = $request->client_name ?? $order->client_name;
-            $order->phone_code = $request->phone_code ?? $order->phone_code ?? '+92';
-            $order->phone_number = $request->phone_number ?? $order->phone_number;
-            $order->delivery_address = $request->delivery_address ?? $order->delivery_address;
-            $order->order_status = $request->order_status;
-            $order->advance_payment = $request->advance_payment ?? $order->advance_payment;
-            $order->quantity = $request->quantity ?? $order->quantity;
-            $order->raw_material = $request->raw_material ?? $order->raw_material;
-            $order->tax = $request->tax ?? $order->tax ;
-            $order->packing = $request->packing ?? $order->packing;
-            $order->profit = $request->profit ?? $order->profit;
-            $order->total_price = $order->raw_material + $order->tax + $order->packing + $order->profit;
+            $order->client_name           = $request->client_name ?? $order->client_name;
+            $order->phone_code            = $request->phone_code ?? $order->phone_code ?? '+92';
+            $order->phone_number          = $request->phone_number ?? $order->phone_number;
+            $order->delivery_address      = $request->delivery_address ?? $order->delivery_address;
+            $order->order_status          = $request->order_status;
+            $order->advance_payment       = $request->advance_payment ?? $order->advance_payment;
+            $order->quantity              = $request->quantity ?? $order->quantity;
+            $order->raw_material          = $request->raw_material ?? $order->raw_material;
+            $order->tax                   = $request->tax ?? $order->tax ;
+            $order->packing               = $request->packing ?? $order->packing;
+            $order->profit                = $request->profit ?? $order->profit;
+            $order->total_price           = $order->raw_material + $order->tax + $order->packing + $order->profit;
 
         $order->save();
         if(!$order->save())
             return sendError('Order not saved',[]);
         }
 
-        $data['order'] = Order::where('id', $order->id)->first();
+        $data['order'] = Order::find($order->id);
         $data ['raw_material'] = 0;
 
         if(!isset($request->order_id)){
 
-            $ingredient = ProductIngredient::where('product_id',$product->id)->pluck('purchase_list_id');
-            if(NULL != $ingredient){
+            $product_ingredients = ProductIngredient::where('product_id',$product->id)->get();
+            foreach($product_ingredients as $product_ingredient){
 
-                $purchase = PurchaseList::whereIn('id',$ingredient);
-                $quantity = clone $purchase;
-                $quantity = $quantity->pluck('quantity')->sum();
+                $purchase_list = PurchaseList::where('admin_ingredient_id',$product_ingredient['admin_ingredient_id'])->where('admin_ingredient_type_id',$product_ingredient['admin_ingredient_type_id'])->first();
 
-                $purchase = $purchase->pluck('unit_price')->sum();
-                }
-                if(NULL != $purchase){
-                    foreach($product->productIngredient as $key => $qt){
-                        if($qt->quantity < $request->quantity)
-                            return sendError('Quantity Over Reached',[]);
+                // return $purchase_list;`
+                if(isset($purchase_list)){
+                    $check = $purchase_list->quantity - ($product_ingredient['quantity'] * $request->quantity);
+                   
+                    if($check<0)
+                        return sendError('Quantity Over Reached',[]);
 
-                    $data ['raw_material'] += $purchase * $qt->quantity * $request->quantity;
+                    $data['raw_material'] += $product_ingredient['quantity'] * $request->quantity * $purchase_list->unit_price;
+
                     if(isset($request->raw_material))
-                        $purchase = PurchaseList::where('id',$ingredient[$key])->decrement('quantity', $qt->quantity * $request->quantity);
-                    }
+                        $purchase_list->quantity = $purchase_list->quantity - ($product_ingredient['quantity'] * $request->quantity);
+                        $purchase_list->save();
                 }
+
+            }
+
         }
 
         return sendSuccess('Order Saved',$data);
@@ -122,7 +124,7 @@ class OrderController extends Controller
 
         $validator = Validator::make($request->all(),[
 
-            'user_id' => 'exists:users,id|numeric',
+            'user_id'  => 'exists:users,id|numeric',
             'order_id' => 'exists:orders,id|numeric',
 
         ]);
@@ -157,8 +159,8 @@ class OrderController extends Controller
         
         $validator = Validator::make($request->all(),[
 
-            'month' => 'required',
-            'year' => 'required',
+            'month'   => 'required',
+            'year'    => 'required',
             'user_id' => 'required|exists:users,id',
         ]);
 
@@ -167,10 +169,10 @@ class OrderController extends Controller
                 return sendError($validator->errors()->all()[0], $data);
         }
 
-        $month_sale = [];
+        $month_sale     = [];
         $month_purchase = [];
-        $sum_order = [];
-        $month = cal_days_in_month(CAL_GREGORIAN,$request->month, $request->year);
+        $sum_order      = [];
+        $month          = cal_days_in_month(CAL_GREGORIAN,$request->month, $request->year);
 
         // dd($i);
 
@@ -190,10 +192,10 @@ class OrderController extends Controller
         }
 
 
-        $data['order'] = $month_order;
-        $data['purchase'] = $month_purchase;
-        $data['purchase_sum'] =  $purchase->pluck('price')->sum();
-        $data['sale_sum'] =  $order->where('order_status','sold')->pluck('total_price')->sum();
+        $data['order']        = $month_order;
+        $data['purchase']     = $month_purchase;
+        $data['purchase_sum'] = $purchase->pluck('price')->sum();
+        $data['sale_sum']     = $order->where('order_status','sold')->pluck('total_price')->sum();
 
 
         return sendSuccess("Data",$data);
